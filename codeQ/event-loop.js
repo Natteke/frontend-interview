f89840ab1e4622f9d8392c68763ddad18421a944
andrey.ponomarenko <andrey.ponomarenko@semrush.com>
2023-02-24 12:17:49 +0100

    fx

--- codeQ/event-loop.js
+++ codeQ/event-loop.js
@@ -1,14 +1,33 @@
-// В каком порядке выведутся цифры?
-
-console.log(3);
-setTimeout(function() {
-    console.log(2);
+setTimeout(() => {
+    console.log('timeout');
 }, 0);
-console.log(1);
 
-// ============================
+Promise.resolve().then(() => {
+    console.log('promise resolved');
+});
 
-Promise.resolve().then(() => console.log(1));
-console.log(2);
-setTimeout(() => console.log(3));
+(async function() {
+    console.log('function');
+})();
+
+console.log('completed');
+
+requestAnimationFrame(() => {
+    console.log('animation frame callback');
+});
+
+
+
+
+
+
+
+/*
+function - несмотря на то, что async возвращает промис, синхронная часть выполняется моментально.
+Только дальнейший ассинхронный код пойдет в таски/микротаски
+completed - синхронный код
+promise resolved - микротаска, приоритетнее тасок
+timeout - таска
+animation frame callback - RAF выполняется перед repaint. Соответственно после синхронного кода и микротасок
+ */
 
